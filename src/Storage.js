9567632bcdda045445d6eb0254810deff32663c9
Olawale <bolawale81@gmail.com>
2022-03-25 13:47:04 +0100

    correct remove function to work

--- src/Storage.js
+++
@@ -1,9 +0,0 @@
-export default class storageManager {
-  static storeData(todoList) {
-    localStorage.setItem('todoList', JSON.stringify(todoList));
-  }
-
-  static getData() {
-    return localStorage.getItem('todoList') ? JSON.parse(localStorage.getItem('todoList')) : [];
-  }
-}
